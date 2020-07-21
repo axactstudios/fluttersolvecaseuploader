b0b8d516118882523794a33aeefa75d1d0548d71
Sanskar <sanskarg348@gmail.com>
2020-07-22 00:10:51 +0530

    flutter uploadding app

---
+++ android/app/src/main/java/com/solvcase/fluttersolvecaseuploader/MainActivity.java
@@ -0,0 +1,6 @@
+package com.solvcase.fluttersolvecaseuploader;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/mainscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Main extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Uploader',
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
