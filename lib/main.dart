c776cb4ec46aa86e50493f4149fdcf7279cef6d3
aditya <aditya29ks@gmail.com>
2020-07-22 11:56:28 -0400

    improved ui

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pet Shop Owner',
+      title: 'SOLVECASE',
       home: MyStatefulWidget(),
     );
   }
